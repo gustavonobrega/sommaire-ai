20311c91e7adbe167f89b278e13fe5638a4104d1
gustavonobrega <gustavo7nobrega@gmail.com>
2025-10-01 15:31:43 -0300

    Update Footer with initial styles

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -2,8 +2,19 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      <h2>Footer</h2>
-    </div>
+    <footer className="py-12">
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+        <p className="text-center text-gray-500 text-sm">
+          Made by{" "}
+          <a
+            href="https://github.com/gustavonobrega"
+            target="_blank"
+            className="font-semibold text-gray-700"
+          >
+            Gustavo
+          </a>
+        </p>
+      </div>
+    </footer>
   );
 }
