c0b8aa39c2d38d243aec179370d2a15bb8e05205
gustavonobrega <gustavo7nobrega@gmail.com>
2025-10-01 08:03:48 -0300

    Add PricingSection to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/home/hero-section";
 import HowItWorksSection from "@/components/home/how-it-works-section";
+import PricingSection from "@/components/home/pricing-section";
 
 export default function Home() {
   return (
     <div className="relative w-full">
       <HeroSection />
       <HowItWorksSection />
+      <PricingSection />
     </div>
   );
 }
